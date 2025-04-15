00b7b61269c5513735104931b317ce87aacb0eab
nabendu82 <nabendu.biswas@gmail.com>
2025-04-15 10:22:23 +0530

    Firebase done

--- src/components/TinderCards.js
+++ src/components/TinderCards.js
@@ -1,20 +1,48 @@
 import { useEffect, useState } from 'react'
 import './TinderCards.css'
 import TinderCard from 'react-tinder-card'
+import db from '../firebase';
 
 const TinderCards = () => {
-    const [people, setPeople] = useState([
-        { name: "Steve", url: "https://randomuser.me/api/portraits/men/9.jpg" },
-        { name: "Elon", url: "https://randomuser.me/api/portraits/men/8.jpg" },
-        { name: "Michele", url: "https://randomuser.me/api/portraits/women/76.jpg" },
-        { name: "Mary", url: "https://randomuser.me/api/portraits/women/25.jpg" }
-    ])
+    // const [people, setPeople] = useState([
+    //     { name: "Steve", url: "https://randomuser.me/api/portraits/men/9.jpg" },
+    //     { name: "Elon", url: "https://randomuser.me/api/portraits/men/8.jpg" },
+    //     { name: "Michele", url: "https://randomuser.me/api/portraits/women/76.jpg" },
+    //     { name: "Mary", url: "https://randomuser.me/api/portraits/women/25.jpg" }
+    // ])
+
+    const [people, setPeople] = useState([]);
+
+    useEffect(() => {
+        db.collection('people').onSnapshot(snapshot => {
+            setPeople(snapshot.docs.map(doc => doc.data()))
+        })
+    },[])
+
+    const onSwipe = (direction) => {
+        console.log('You swiped: ' + direction)
+    }
+
+    const onCardLeftScreen = (myIdentifier) => {
+        console.log(myIdentifier + ' left the screen')
+    }
 
     return (
         <div className='tinderCards'>
-            {people.map(person => (
-                <h1>{person.name}</h1>
-            ))}
+            <div className="tinderCards__container">
+                {people.map(person => (
+                    <TinderCard
+                        className='swipe'
+                        onSwipe={onSwipe}
+                        onCardLeftScreen={() => onCardLeftScreen(person.name)}
+                        preventSwipe={['up', 'down']}
+                    >
+                        <div style={{ backgroundImage: `url(${person.url})` }} className="card">
+                            <h3>{person.name}</h3>
+                        </div>
+                    </TinderCard>
+                ))}
+            </div>
         </div>
     )
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyDMMklpu9zNXKJq2tS5qkWUKVMWJ82fLZ4",
     authDomain: "tinder-hashout.firebaseapp.com",
@@ -6,3 +8,8 @@ const firebaseConfig = {
     messagingSenderId: "308478072241",
     appId: "1:308478072241:web:bc103f000555cb07bf5990"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+
+export default db
